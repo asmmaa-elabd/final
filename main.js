db1dcaca3d98c80bb10e528bcb5aa02327036fe0
asmmaa-elabd <111054010+asmmaa-elabd@users.noreply.github.com>
2024-08-27 20:52:23 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+    
+    // $('#button').click(function(){
+
+    //     if ($('body').css('background-color') === 'rgb(255,255,255'){
+    //         $('body').css('background-color','black');
+
+    //     } else{
+    //         $('body').css('background-color','white');
+            
+    //     }
+    // })
+
+
+    
+
+
+    
+    $('#btn1').click(function() {
+   
+   
+   
+       $(' html , body ').animate({scrollTop : 0} , 800)
+   
+   })
+   
+   
+   })
+   $(document).ready(function() {
+    $('#button').click(function(){
+
+        if ($('body').css('background-color') === 'rgb(255,255,255'){
+            $('body').css('background-color','black');
+
+        } else{
+            $('body').css('background-color','white');
+            
+        }
+    })
+
+
+
+   })
+
+   
+
+
+
+
+
+
